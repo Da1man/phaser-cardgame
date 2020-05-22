b6728a06b305dbef9c9c5768828c2e8b91ce10ba
Da1man <chaos4ever@mail.ru>
2020-05-22 10:20:38 +0300

    first commit

---
+++ assets/scripts/GameScene.js
@@ -0,0 +1,197 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super("Game");
+    }
+
+    preload() {
+        // 1. Загрузить бэкграунд
+        this.load.image('bg', 'assets/sprites/background.png')
+        this.load.image('card', 'assets/sprites/card.png')
+
+        this.load.image('card1', 'assets/sprites/card1.png')
+        this.load.image('card2', 'assets/sprites/card2.png')
+        this.load.image('card3', 'assets/sprites/card3.png')
+        this.load.image('card4', 'assets/sprites/card4.png')
+        this.load.image('card5', 'assets/sprites/card5.png')
+
+        this.load.audio('theme', 'assets/sounds/theme.mp3')
+        this.load.audio('card', 'assets/sounds/card.mp3')
+        this.load.audio('complete', 'assets/sounds/complete.mp3')
+        this.load.audio('success', 'assets/sounds/success.mp3')
+        this.load.audio('timeout', 'assets/sounds/timeout.mp3')
+    }
+
+    createText() {
+        this.timeoutText = this.add.text(10, 300, '', {
+            font: '36px CurseCasual',
+            fill: '#ffffff',
+        })
+    }
+
+    onTimerTick(){
+        this.timeoutText.setText('Time: ' + this.timeout)
+
+        if (this.timeout <= 0) {
+            this.timer.paused = true
+            this.sounds.timeout.play({volume: 0.5})
+            this.restart();
+        } else {
+            --this.timeout
+        }
+        
+    }
+
+    createTimer() {
+        this.timer = this.time.addEvent({
+            delay: 1000,
+            callback: this.onTimerTick,
+            callbackScope: this,
+            loop: true,
+        });
+    }
+
+    createSounds(){
+        this.sounds = {
+            card: this.sound.add('card'),
+            complete: this.sound.add('complete'),
+            success: this.sound.add('success'),
+            timeout: this.sound.add('timeout'),
+            theme: this.sound.add('theme'),
+        }
+        this.sounds.theme.play({volume: 0.01})
+        
+    }
+
+    create() {
+        this.timeout = config.timeout;
+        this.createSounds();
+        this.createTimer();
+        this.createBackground();
+        this.createText();
+        this.createCards();
+        this.start();
+    }
+
+    restart() {
+        let count = 0;
+        let onCardMoveComplete = () => {
+            ++count;
+            if (count >= this.cards.length) {
+                this.start();
+            }
+        }
+        this.cards.forEach(card => {
+            card.move({
+                x: this.sys.game.config.width + card.width,
+                y: this.sys.game.config.height + card.height,
+                delay: card.position.delay,
+                callback: onCardMoveComplete
+            })
+        })
+        
+    }
+
+    start() {
+        this.initcard
+        this.timeout = config.timeout;
+        this.openedCard = null;
+        this.opendedCardsCount = 0;
+        this.timer.paused = false
+        this.initCards();
+        this.showCards();
+    }
+
+    initCards() {
+        let positions = this.getCardPositions();
+
+        this.cards.forEach(card => {
+            card.init(positions.pop())
+        })
+    }
+
+    showCards() {
+        this.cards.forEach(card => {
+            card.depth = card.position.delay
+            card.move({
+                x: card.position.x,
+                y: card.position.y,
+                delay: card.position.delay
+            })
+        })
+    }
+
+    createBackground() {
+        this.add.sprite(0, 0, 'bg').setOrigin(0, 0)
+    }
+
+    createCards() {
+        this.cards = []
+
+        for(let value of config.cards) {
+            for (let i = 0; i < 2; i++){
+                this.cards.push(new Card(this, value))
+            }
+        }
+        this.input.on('gameobjectdown', this.onCardClicked, this)
+    }
+
+    onCardClicked(pointer, card) {
+        if (card.opened) {
+            return false
+        }
+
+        this.sounds.card.play({volume: 0.5})
+
+        if (this.openedCard){
+            // уже есть открытая карта
+            if(this.openedCard.value === card.value) {
+                // картинки равны - запомнить
+                this.sounds.success.play({volume: 0.5})
+                
+                this.openedCard = null;
+                this.opendedCardsCount += 1;
+            } else {
+                // картинки разные - скрыть прошлую
+                this.openedCard.close();
+                this.openedCard = card
+            }
+        } else {
+            // еще нет открытой карты
+            this.openedCard = card
+
+        }
+
+        card.open(() => {
+            if (this.opendedCardsCount === this.cards.length / 2) {
+                this.sounds.complete.play({volume: 0.5})
+                this.restart();
+            }
+        });
+
+        
+
+    }
+
+    getCardPositions() {
+        let positions = [];
+        let cardTexture = this.textures.get('card').getSourceImage();
+        let cardWidth = cardTexture.width + 4;
+        let cardHeight = cardTexture.height + 4;
+        let offSetX = (this.game.config.width - cardWidth * config.cols) / 2 + cardWidth / 2 ;
+        let offSetY = (this.game.config.height - cardHeight * config.rows) / 2 + cardHeight / 2 ;
+        
+        let id = 0;
+        for (let row = 0; row < config.rows; row++){
+            for (let col = 0; col < config.cols; col++) {
+                ++id;
+                positions.push({
+                    x: offSetX + col * cardWidth,
+                    y: offSetY + row * cardHeight,
+                    delay: id * 100,
+                })
+            }
+        }
+        return Phaser.Utils.Array.Shuffle(positions)
+    }
+
+}
